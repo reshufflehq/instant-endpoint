f3fed24b79484fdaf1d712fa86faec67a1227d9d
Avner Braverman <avner@binaris.com>
2019-11-28 14:57:01 -0800

    Simple API endpoint POST handler and frontend

--- backend/_handler.js
+++ backend/_handler.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+app.post('/api', (req, res) => {
   return res.status(200).send('Ok');
 });
 
