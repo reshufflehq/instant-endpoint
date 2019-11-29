3f0c0fb3254fd05bae646b3ecbe06ad7cf14a0bc
Avner Braverman <avner@binaris.com>
2019-11-29 15:13:17 -0800

    Add GET API endpoint

--- backend/_handler.js
+++ backend/_handler.js
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.post('/api', (req, res) => {
-  return res.status(200).send('Ok');
+  return res.status(200).send('Post it!');
+});
+
+app.get('/api', (req, res) => {
+  return res.status(200).send('Get it?');
 });
 
 export default app;
